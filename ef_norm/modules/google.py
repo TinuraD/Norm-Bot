6cbba3ce405140febcc6661a5950c1dfed3c3a5c
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-27 18:59:25 +0530

    Create google.py

---
+++ ef_norm/modules/google.py
@@ -0,0 +1,89 @@
+# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+
+__MODULE__ = "Google"
+__HELP__ = "/google For search"
+
+import os
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+API = "https://api.abirhasan.wtf/google?query="
+
+
+Bot = Client(
+    "Google-Search-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+START_TEXT = """Hello {}
+I am a google search bot.
+> `I can search from google. Use me in inline.`
+Made by @FayasNoushad"""
+
+JOIN_BUTTON = [
+    InlineKeyboardButton(
+        text='⚙ Join Updates Channel ⚙',
+        url='https://telegram.me/FayasNoushad'
+    )
+]
+
+
+@Bot.on_message(filters.private & filters.command(["google"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([JOIN_BUTTON]),
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_inline_query()
+async def inline(bot, update):
+    results = google(update.query)
+    answers = []
+    for result in results:
+        answers.append(
+            InlineQueryResultArticle(
+                title=result["title"],
+                description=result["description"],
+                input_message_content=InputTextMessageContent(
+                    message_text=result["text"],
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton(text="Link", url=result["link"])],
+                        JOIN_BUTTON
+                    ]
+                )
+            )
+        )
+    await update.answer(answers)
+
+
+def google(query):
+    r = requests.get(API + requote_uri(query))
+    informations = r.json()["results"]
+    results = []
+    for info in informations:
+        text = f"**Title:** `{info['title']}`"
+        text += f"\n**Description:** `{info['description']}`"
+        text += f"\n\nMade by @FayasNoushad"
+        results.append(
+            {
+                "title": info['title'],
+                "description": info['description'],
+                "text": text,
+                "link": info['link']
+            }
+        )
+    return results
+
+
+Bot.run()
