475a3bb04750cac1376a2f9541dcfcc378cf2e64
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-27 19:32:11 +0530

    Update google.py

--- ef_norm/modules/google.py
+++ ef_norm/modules/google.py
@@ -1,17 +1,13 @@
 __help__ = """
 *Google:*
-✪ Daisy can mute members who are not subscribed your channel until they subscribe
-✪ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
-*Setup*
-1) First of all add me in the group as admin with ban users permission and in the channel as admin.
-Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
- 
-*Commmands*
-✪ /ForceSubscribe - To get the current settings.
-✪ /ForceSubscribe no/off/disable - To turn of ForceSubscribe.
-✪ /ForceSubscribe {channel username} - To turn on and setup the channel.
-✪ /ForceSubscribe clear - To unmute all members who muted by me.
-Note: /FSub is an alias of /ForceSubscribe
+Use @efgooglebot to search
  
 """
 __mod_name__ = "Google"
+
+__help__ = """
+*Google:*
+Use @efgooglebot to search
+ 
+"""
+__mod_name__ = "Youtube Search"
