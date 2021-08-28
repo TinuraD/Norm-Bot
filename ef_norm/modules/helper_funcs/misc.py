fa94373e9968f758f3eaf123a6f81d8adc86e6d4
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-28 11:35:05 +0530

    Update misc.py

--- ef_norm/modules/helper_funcs/misc.py
+++ ef_norm/modules/helper_funcs/misc.py
@@ -60,12 +60,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 10)
+    max_num_pages = ceil(len(pairs) / 5)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 10:
-        pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * 5:5 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("ආපසු", callback_data="aboutmanu_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
