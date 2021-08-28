9c7f1a672c6f628304dfeaba9d9057b09552c27a
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-28 11:27:41 +0530

    Update misc.py

--- ef_norm/modules/helper_funcs/misc.py
+++ ef_norm/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 10:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("↪️ආපසු ↪️", callback_data="aboutmanu_back"),
+                EqInlineKeyboardButton("ආපසු", callback_data="aboutmanu_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("↪️ආපසු ↪️", callback_data="aboutmanu_back")]]
+        pairs += [[EqInlineKeyboardButton("ආපසු", callback_data="aboutmanu_back")]]
 
     return pairs
 
