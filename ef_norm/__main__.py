90a69053690aaaa7cab6b982cd002fadbb4bd072
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-28 10:07:05 +0530

    Update __main__.py

--- ef_norm/__main__.py
+++ ef_norm/__main__.py
@@ -290,7 +290,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "*⚊❮❮❮❮ ｢  Help  for  {}  module 」❯❯❯❯⚊*\n".format(
+                "{} භාවිතා කරන ආකාරය.".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
