0778f0da6e7dc0fba4a562f6a5ec74ff890c2ac7
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-29 17:22:21 +0530

    Update send.py

--- ef_norm/modules/send.py
+++ ef_norm/modules/send.py
@@ -1,20 +1,78 @@
-from telegram.ext import run_async
+from pyrogram import filters
+from pyrogram.types import Message
 
-from ef_norm import dispatcher
-from ef_norm.modules.disable import DisableAbleCommandHandler
-from ef_norm.modules.helper_funcs.alternate import send_message
-from ef_norm.modules.helper_funcs.chat_status import user_admin
+from ef_norm import bot
+from pyrogram.errors import RPCError
+
+import functools
+from typing import Callable, Coroutine, Dict, List, Tuple, Union
+
+def is_admin(func):
+    @functools.wraps(func)
+    async def oops(client,message):
+        is_admin = False
+        try:
+            user = await message.chat.get_member(message.from_user.id)
+            admin_strings = ("creator", "administrator")
+            if user.status not in admin_strings:
+                is_admin = False
+            else:
+                is_admin = True
+
+        except ValueError:
+            is_admin = True
+        if is_admin:
+            await func(client,message)
+        else:
+            await message.reply("Only Admins can execute this command!")
+    return oops
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 
 
-@run_async
-@user_admin
-def send(update, context):
-    args = update.effective_message.text.split(None, 1)
-    creply = args[1]
-    send_message(update.effective_message, creply)
+@bot.on_message(
+    filters.command("send") & ~filters.edited & ~filters.bot)
+@is_admin
+async def send(client, message):
+    if message.reply_to_message:
+            reply = message.reply_to_message
+            try:
+                await reply.copy(message.chat.id)
+            except RPCError as i:
+                await message.reply(i)
+                return
 
+    else:
+        args = get_text(message)
+        await client.send_message(message.chat.id, text=args)
 
-ADD_CCHAT_HANDLER = DisableAbleCommandHandler("snd", send)
-dispatcher.add_handler(ADD_CCHAT_HANDLER)
-__command_list__ = ["snd"]
-__handlers__ = [ADD_CCHAT_HANDLER]
+@bot.on_message(
+    filters.command("edit") & ~filters.edited & ~filters.bot)
+@is_admin
+async def loltime(client, message):
+    lol = await message.reply("Processing please wait")
+    cap = get_text(message)
+    if not message.reply_to_message:
+        await lol.edit("reply to any message to edit caption")
+    reply = message.reply_to_message
+    try:
+        await reply.copy(message.chat.id,caption= cap)
+        await lol.delete()
+    except RPCError as i:
+        await lol.edit(i)
+        return
+
+@bot.on_message(
+    filters.command("start") & ~filters.edited & ~filters.bot)    
+async def lel(client, message):
+    lol = await message.reply("forward any file to get it without tag. \nCheck /help to know more")  
