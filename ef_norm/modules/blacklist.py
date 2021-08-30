80331d7a050c7389999d3981943594e7e6dfb7e2
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-08-30 16:10:54 +0530

    Update blacklist.py

--- ef_norm/modules/blacklist.py
+++ ef_norm/modules/blacklist.py
@@ -449,16 +449,16 @@ def __stats__():
     )
 
 
-__mod_name__ = "Blacklists ⛔️"
+\__mod_name__ = "Blacklists ⛔️"
 
 __help__ = """
-Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+ඔයාගේ group එකේ දාන සමහර වචන දාන එක නවත්තන්න මේක පාවිච්චි කරන්න පුළුවන්.
 *NOTE*: Blacklists do not affect group admins.
- • `/blacklist`*:* View the current blacklisted words.
+ • `/blacklist`*:* දැනට blacklisted කරපු වචන.
 Admin only:
- • `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- • `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
+ • `/addblacklist <වචනය> - වචනයක් blacklist කරන්න.
+ • `/unblacklist <triggers> - වචනයක් unblacklist කරන්න.
+ • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute> - Blacklisted වචන දැම්මාම දෙන දඩුවම.
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
     "blacklist", blacklist, pass_args=True, admin_ok=True
