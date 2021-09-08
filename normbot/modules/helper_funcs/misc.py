c2cfdbb9949744f03d928f43e3a571f0c06c6d99
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-08 20:51:13 +0530

    Update misc.py

--- normbot/modules/helper_funcs/misc.py
+++ normbot/modules/helper_funcs/misc.py
@@ -66,8 +66,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 5:
         pairs = pairs[modulo_page * 4:4 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton("Next", callback_data="{}_next({})".format(prefix, modulo_page)))] + [
+            (EqInlineKeyboardButton("Back", callback_data="{}_prev({})".format(prefix, modulo_page)))] + [
             (EqInlineKeyboardButton("Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Close", callback_data="aboutmanu_back"),
              EqInlineKeyboardButton("Next", callback_data="{}_next({})".format(prefix, modulo_page)))]
