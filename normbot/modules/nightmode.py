b21c24ae7213bc70dcbdee3271f898e77da3f6d3
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-08 19:43:51 +0530

    Update nightmode.py

--- normbot/modules/nightmode.py
+++ normbot/modules/nightmode.py
@@ -1,6 +1,6 @@
 import os
 
-from DewmiBot.modules.sql.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
+from normbot.modules.sql.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
 from telethon.tl.types import ChatBannedRights
 from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from telethon import functions
