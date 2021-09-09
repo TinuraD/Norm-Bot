f665c95fda0e96e7d818b5226ea3131c06266829
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-09 07:59:40 +0530

    More Tools 🧰

---
+++ normbot/modules/moretools.py
@@ -0,0 +1,28 @@
+moretool = f"""
+** GPS **
+ • /gps <හොයන්න ඕන තැන>  - අපි දෙන තැන Map එක බලන්න.
+ 
+ ** Blue Cleaner **
+ • /cleanblue on : Turn bluetext cleaner on
+ • /cleanblue off : Turn bluetext cleaner off
+ 
+ ** Send **
+ • /send <message> : Bot ගෙන් message දාන්න.
+ • /edit <reply to media> : File එකක media edit කරන්න.
+ 
+** Grammer **
+ • /t <reply> : Grammer හරි ගස්සන්න
+ 
+** Image Tools**
+ • /img <ඕන image එක search කරන්න>: Photo කරන්න
+ • /getqr <photo එකකට reply කරන්න >: Photo එකට QR code එකක් හදන්න
+ • /makeqr <Link එක දෙන්න>: Make QR code එකක් හදන්න.
+ 
+** Text Style කරන්න **
+ • /weebify : Weebify Text
+ • /square : square Text
+ • /blue : Blues text
+ 
+** More **
+ - /phone <Phone නම්බරේ> : Phone නම්බර් Track කරන්න.
+"""
