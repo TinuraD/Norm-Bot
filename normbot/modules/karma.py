ff5393a7262a98eba7c6acbafd3fea0b534356ea
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-18 19:17:10 +0530

    Update karma.py

--- normbot/modules/karma.py
+++ normbot/modules/karma.py
@@ -26,9 +26,9 @@ from typing import Dict, Union
 from pyrogram import filters
 
 from DaisyX.db.mongo_helpers.karma import is_karma_on, karma_off, karma_on
-from DaisyX.function.pluginhelpers import member_permissions
+from normbot.function import member_permissions
 from DaisyX.services.mongo2 import db
-from DaisyX.services.pyrogram import pbot as app
+from normbot import pbot as app
 
 karmadb = db.karma
 karma_positive_group = 3
