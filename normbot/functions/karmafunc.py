ae1a52e1ef92a4c270411978fa0a0f9596f254ac
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-20 21:53:15 +0530

    Update karmafunc.py

--- normbot/functions/karmafunc.py
+++ normbot/functions/karmafunc.py
@@ -221,3 +221,30 @@ def adminsOnly(permission):
         return subFunc2
 
     return subFunc
+
+n = "\n"
+w = " "
+
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+    title: str,
+    body: dict,
+    indent: int = 2,
+    underline: bool = False,
+) -> str:
+
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        text += (
+            indent * w
+            + bold(key)
+            + ((value[0] + n) if isinstance(value, list) else mono(value))
+        )
+    return text
