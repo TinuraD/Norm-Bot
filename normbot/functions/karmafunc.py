43e061d13b852ab64b8a8b5ea32ba6722cd309b1
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-22 16:54:26 +0530

    😭😭

--- normbot/functions/karmafunc.py
+++ normbot/functions/karmafunc.py
@@ -112,6 +112,24 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
     user_id = int(user_id)
     return user_id
 
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+
+    lines = text.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    else:
+        result.append(small_msg)
+
+    return result
+
 def capture_err(func):
     @wraps(func)
     async def capture(client, message, *args, **kwargs):
