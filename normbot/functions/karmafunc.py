2d991bbb4ce6fa3046dffed83b18f231bbfc3872
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-22 15:58:02 +0530

    Update karmafunc.py

--- normbot/functions/karmafunc.py
+++ normbot/functions/karmafunc.py
@@ -89,7 +89,7 @@ async def karma_on(chat_id: int):
     is_karma = await is_karma_on(chat_id)
     if is_karma:
         return
-    return await karmadb.delete_one({"chat_id_toggle": chat_id})
+    return karmadb.delete_one({"chat_id_toggle": chat_id})
 
 
 async def karma_off(chat_id: int):
