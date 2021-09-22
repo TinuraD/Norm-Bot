13ebd8ebb0b91e168a2fd5764dc53c99dc8cad1d
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-22 17:00:59 +0530

    Update karmafunc.py

--- normbot/functions/karmafunc.py
+++ normbot/functions/karmafunc.py
@@ -130,34 +130,6 @@ def split_limits(text):
 
     return result
 
-def capture_err(func):
-    @wraps(func)
-    async def capture(client, message, *args, **kwargs):
-        try:
-            return await func(client, message, *args, **kwargs)
-        except ChatWriteForbidden:
-            await pbot.leave_chat(message.chat.id)
-            return
-        except Exception as err:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
-            error_feedback = split_limits(
-                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
-                    0 if not message.from_user else message.from_user.id,
-                    0 if not message.chat else message.chat.id,
-                    message.text or message.caption,
-                    "".join(errors),
-                ),
-            )
-            for x in error_feedback:
-                await pbot.send_message(JOIN_LOGGER, x)
-            raise err
-
-    return capture
 
 async def authorised(func, subFunc2, client, message, *args, **kwargs):
     chatID = message.chat.id
