3f7e18be9f5a958a5f8e19f7f0b530580a7331e9
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-23 15:28:31 +0530

    Update __init__.py

--- normbot/__init__.py
+++ normbot/__init__.py
@@ -87,7 +87,6 @@ if ENV:
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
 
     try:
         WHITELIST_CHATS = set(
