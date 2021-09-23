4e72115a8df26341785365a95cca2c852827be0f
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-23 07:59:24 +0530

    System Stats.

--- normbot/functions/stats.py
+++ normbot/functions/stats.py
@@ -6,11 +6,39 @@ import time
 import psutil
 from pyrogram import filters
 
-from DewmiBot import bot_start_time
-from DewmiBot.utils import formatter
+bot_start_time = time.time()
 
-# Stats Module
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
 
+
+async def convert_seconds_to_minutes(seconds: int):
+    seconds = int(seconds)
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - bot_start_time)
@@ -19,7 +47,8 @@ async def bot_sys_stats():
     disk = psutil.disk_usage("/").percent
     process = psutil.Process(os.getpid())
     stats = f"""
-root@Supunma:~$ szrosebot
+
+root@TinuraD:~$ Norm
 ------------------
 UPTIME: {formatter.get_readable_time((bot_uptime))}
 BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
