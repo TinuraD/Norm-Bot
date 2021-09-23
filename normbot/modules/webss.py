69c94b382598e8bdcbbb11a1679a6714ec407c2e
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-23 07:26:05 +0530

    Update webss.py

--- normbot/modules/webss.py
+++ normbot/modules/webss.py
@@ -7,24 +7,27 @@ from pyrogram.types import Message
 from normbot import pbot
 
 __MODULE__ = "WebSS 📱"
-__HELP__ = "/webss | .webss [URL] - Take A Screenshot Of A Webpage"
+__HELP__ = f"""
+• /webss [link] - වෙබ්පිටුවක screenshot එකක් ගැනීම සදහා.
+• මෙහිදී ඔබ link එක ලබා දීමෙදී http:// හෝ https:// නිවැරදිව ලබා දිය යුතුය.
+"""
 
 
 @pbot.on_message(filters.command("webss"))
 async def take_ss(_, message: Message):
     try:
         if len(message.command) != 2:
-            return await message.reply_text("Give A Url To Fetch Screenshot.")
+            return await message.reply_text("Screenshot එකක් ලබා ගැනීමට එකක් ලබා දෙන්න.")
         url = message.text.split(None, 1)[1]
-        m = await message.reply_text("**Taking Screenshot**")
-        await m.edit("**Uploading**")
+        m = await message.reply_text("Screenshot එක ගනිමින්")
+        await m.edit("උඩුගත කරමින්")
         try:
             await message.reply_photo(
                 photo=f"https://webshot.amanoteam.com/print?q={url}",
                 quote=False,
             )
         except TypeError:
-            return await m.edit("No Such Website.")
+            return await m.edit("එවැනි වෙබ්පිටුවක් නොමැත.")
         await m.delete()
     except Exception as e:
         await message.reply_text(str(e))
