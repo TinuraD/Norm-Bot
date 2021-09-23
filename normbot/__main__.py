f9cdd1eca96bdde3635b106dbb0fb10480d80cac
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-23 07:07:09 +0530

    Update __main__.py

--- normbot/__main__.py
+++ normbot/__main__.py
@@ -214,10 +214,13 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            " I'm online!!😊\n<b>Up since:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton(text="Help", url ="http://t.me/sznormbot?start=help")]],
+            ),
         )
 
 
