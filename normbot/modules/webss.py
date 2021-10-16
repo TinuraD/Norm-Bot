17687998dac27d7332dbac5664ab038cb40837f4
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-10-16 14:17:42 +0530

    Update webss.py

--- normbot/modules/webss.py
+++ normbot/modules/webss.py
@@ -1,34 +1,23 @@
-# Ported From William Butcher Bot
-# Ported by Tinura Dinith for Norm
-
 from pyrogram import filters
 from pyrogram.types import Message
 
-from normbot import pbot
+from normbot import pbot as bolt
 
-@pbot.on_message(filters.command("webss"))
+@bolt.on_message(filters.command("webss"))
 async def take_ss(_, message: Message):
     try:
         if len(message.command) != 2:
-            return await message.reply_text("Screenshot එකක් ලබා ගැනීමට link එකක් ලබා දෙන්න.")
+            return await message.reply_text("Give A Url To Fetch Screenshot.")
         url = message.text.split(None, 1)[1]
-        m = await message.reply_text("Screenshot එක ගනිමින්")
-        await m.edit("උඩුගත කරමින්")
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
         try:
             await message.reply_photo(
-                photo=f"https://webshot.amanoteam.com/print?q={url}",
+                photo=f"https://webshot.deam.io/{url}",
                 quote=False,
             )
         except TypeError:
-            return await m.edit("එවැනි වෙබ්පිටුවක් නොමැත.")
+            return await m.edit("No Such Website.")
         await m.delete()
     except Exception as e:
-        await message.reply_text(str(e))
-      
-__help__ = """
-• /webss [link] - වෙබ්පිටුවක screenshot එකක් ගැනීම සදහා.
-• මෙහිදී ඔබ link එක ලබා දීමෙදී http:// හෝ https:// නිවැරදිව ලබා දිය යුතුය.
- උදා:- `/webss https://www.google.com/`
-"""
-
-__mod_name__ = "WebSS 📱"  
+        await message.reply_text(str(e))        
