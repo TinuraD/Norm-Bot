98f49ac58afa00f2c14adce74ac816b3fd0d3d13
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-11-10 12:30:02 +0530

    Update chatbot.py

--- normbot/modules/chatbot.py
+++ normbot/modules/chatbot.py
@@ -56,8 +56,6 @@ async def chatbot(_, message):
     chat_id = message.chat.id
     if not (is_chatbot_indb(str(message.chat.id))):
         return
-    if message.reply_to_message.from_user.id != BOT_ID:
-        return
     if message.text[0] == "/":
         return
     await pbot.send_chat_action(message.chat.id, "typing")
